9b40fed7237fe74f14b53b44f2b2e34a2c79468a
CLICKNEXT\Saiswan.w <sai@clicknext.com>
2025-02-19 10:40:01 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dev-jhom/',
+  base: '/devjhom-site/',
   plugins: [vue()],
   resolve: {
     alias: {
